30d394d64c315087a4645225b95216c4bdc6b74f
Mord0reK <marcel.stosio51@gmail.com>
2025-11-19 13:18:37 +0100

    Lekcja 9 - Formularze JS

--- Lekcja 9 - Formularz JS/script.js
+++ Lekcja 9 - Formularz JS/script.js
@@ -4,4 +4,24 @@ function formularz()
     wynik.innerHTML = "Imię: " + document.getElementById("imie").value + "<br>";
     wynik.innerHTML += "Nazwisko: " + document.getElementById("nazwisko").value + "<br>";
     wynik.innerHTML += "Klasa: " + document.getElementById("klasa").value;
+    wynik.innerHTML += "<br>Liczba 1: " + czypierwsza(document.getElementById("liczba1").value);
+    wynik.innerHTML += "<br>Liczba 2: " + dwojkowy(Number(document.getElementById("liczba2").value));
+}
+
+function czypierwsza(liczba)
+{
+    for (let i = 2; i < liczba; i++)
+    {
+        if (liczba % i === 0)
+        {
+            return "Nie jest pierwsza";
+        }
+    }
+    return "Pierwsza";
+}
+
+function dwojkowy(liczba)
+{
+    var dwojkowa = liczba.toString(2);
+    return dwojkowa;
 }
