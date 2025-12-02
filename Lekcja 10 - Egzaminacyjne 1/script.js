eb5bf48690a32253e001ffa29c3ad0661a3d1e01
Mord0reK <marcel.stosio51@gmail.com>
2025-12-02 18:40:23 +0100

    Nie chce mi sie już - ide spać

---
+++ Lekcja 10 - Egzaminacyjne 1/script.js
@@ -0,0 +1,21 @@
+function funkcja()
+{
+    let ksztalt = document.getElementById("ksztalt").value
+
+    switch(ksztalt)
+    {
+        case "1": wybrany = "cytryna"; break
+        case "2": wybrany = "liść"; break
+        case "3": wybrany = "banan"; break
+        default: wybrany = "inny"; break
+    }
+
+    document.getElementById("zamowienie").innerHTML = `<p>Twoje zamówienie to cukierek ${wybrany}</p>`
+
+    let czerwony = document.getElementById("R").value
+    let zielony = document.getElementById("G").value
+    let niebieski = document.getElementById("B").value
+
+    console.log("rgb(" + czerwony + "," + zielony + "," + niebieski + ")")
+    document.getElementById("przycisk").style.backgroundColor = "rgb(" + czerwony + "," + zielony + "," + niebieski + ")"
+}
