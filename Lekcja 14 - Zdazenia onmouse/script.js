3d2aa6d9ffee236730ef12a2d60e40bef5860da8
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2026-03-12 08:43:34 +0000

    Najezdzane itp

---
+++ Lekcja 14 - Zdazenia onmouse/script.js
@@ -0,0 +1,51 @@
+function ramkawl()
+{
+    document.getElementById("zad1").className = "ramka";
+}
+
+function ramkawyl()
+{
+    document.getElementById("zad1").classList = "";
+}
+
+function przyciskwl()
+{
+    document.getElementById("przycisk").style = "background-color: lightblue;"
+    document.getElementById("przycisk").value = "najechany"
+}
+
+function przyciskwyl()
+{
+    document.getElementById("przycisk").style = "background-color: white;"
+    document.getElementById("przycisk").value = "przycisk"
+}
+
+function powieksz1()
+{
+    document.getElementById("obrazek1").style = "width: 150px; height: 150px;"
+}
+
+function powieksz2()
+{
+    document.getElementById("obrazek2").style = "width: 150px; height: 150px;"
+}
+
+function powieksz3()
+{
+    document.getElementById("obrazek3").style = "width: 150px; height: 150px;"
+}
+
+function pomniejsz1()
+{
+    document.getElementById("obrazek1").style = "width: 100px; height: 100px;"
+}
+
+function pomniejsz2()
+{
+    document.getElementById("obrazek2").style = "width: 100px; height: 100px;"
+}
+
+function pomniejsz3()
+{
+    document.getElementById("obrazek3").style = "width: 100px; height: 100px;"
+}
